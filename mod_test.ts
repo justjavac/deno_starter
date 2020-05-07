b729440fc560419243f00e396cd740089b51e40e
迷渡 <justjavac@gmail.com>
2020-05-07 14:53:31 +0800

    bump deno v1.1.0-rc1
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@v1.0.0-rc1/testing/asserts.ts";
 
 import starter from "./mod.ts";
 
