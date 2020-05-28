ba5d516cd7d7449b81a82684fa1374515a09bbbb
Rauf Islam <31735267+ItsRauf@users.noreply.github.com>
2020-05-27 20:27:15 -0400

    Changed version number of assert import (#4)
    Fixes https://github.com/denomod/deno_starter/issues/3

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@v1.0.0-rc1/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@v0.53.0/testing/asserts.ts";
 
 import starter from "./mod.ts";
 
