ae3bd1f839f913cf728d930590c0a4da90c50e52
迷渡 <justjavac@gmail.com>
2020-08-11 09:35:53 +0800

    upgrade deno std 0.64.0

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.63.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.64.0/testing/asserts.ts";
 
 import starter from "./mod.ts";
 
