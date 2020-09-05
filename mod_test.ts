c574c9baa93ab6ebaec766de505eefb88a0196fd
迷渡 <justjavac@gmail.com>
2020-09-05 23:28:42 +0800

    upgrade stdlib to v0.68.0 (#7)

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.68.0/testing/asserts.ts";
 
 import starter from "./mod.ts";
 
