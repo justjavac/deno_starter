ee8f837fd9fc09820419058da327480833ab8171
迷渡 <justjavac@gmail.com>
2022-08-04 08:11:45 +0800

    upgrade std@0.150 (#12)

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.150.0/testing/asserts.ts";
 
 import starter from "./mod.ts";
 
